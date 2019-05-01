8c5566a742c2e2444b10bb9a4102c8f8b21ad39e
Nick Knoebber <nknoebber@ics-llc.net>
2019-05-01 13:31:12 -0500

    Additional Packages
     - Add origami
     - Add go-mode
     - Add magit
     - Add evil-leader

--- init.el
+++ init.el
@@ -9,8 +9,11 @@
 ;; C-x s Save all buffers
 ;; C-h ? help
 ;; C-h k - help about a hotkey combo
-;; C-h f - help for a functio
+;; C-h f - help for a function
 ;; M-j Start a new line with a comment
+;; To install config
+;; list-package RET package-install use-package RET
+;; emacs-helm: C-j to autocomplete candidate
 
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa"))
@@ -26,12 +29,44 @@
 ;; Always download packages that are not found
 (setq use-package-always-ensure t)
 
+;; Setup helm
+(use-package helm
+  :config
+    (require 'helm-config)
+    (helm-mode 1)
+    (helm-autoresize-mode t))
+
+;; Setup magit
+(use-package magit)
+(use-package evil-magit
+  :config
+  (require 'evil-magit))
+
+;; Setup evil leader
+(use-package evil-leader
+  :config
+  (global-evil-leader-mode)
+  (evil-leader/set-leader "SPC")
+  (evil-leader/set-key
+    "RET" 'eshell
+    "x" 'helm-M-x
+    "b" 'helm-mini
+    "f" 'helm-find-files
+    "n" 'rename-buffer
+    "n" 'rename-buffer
+    "gs" 'magit-status
+    "k" (lambda () (interactive) (kill-buffer nil))
+    "r" (lambda() (interactive) (load-file "~/.emacs.d/init.el"))
+    "e" (lambda() (interactive) (find-file "~/.emacs.d/init.el"))
+    ;; C-i is tab
+    "C-i" (lambda() (interactive) (switch-to-buffer nil))))
+
+;; Setup evil
 (use-package evil
   :config 
     (require 'evil)
     (evil-mode t))
 
-;; Inserted by Evil Mode, don't edit:
 '(package-selected-packages (quote (evil-visual-mark-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -49,16 +84,13 @@
    (quote
     (spaceline spacemacs-theme use-package helm evil-visual-mark-mode))))
 
-(use-package helm
-  :bind (("M-x" . helm-M-x)
-	 ("C-x b" . helm-mini)
-	 ("C-x C-f" . helm-find-files))
+;; Setup origami
+(use-package origami
   :config
-    (require 'helm-config)
-    (helm-mode 1)
-    (helm-autoresize-mode t))
+  (require `origami)
+  (origami-mode))
 
-
+;; Setup theme and powerline
 (use-package spacemacs-theme
   :defer t
   :init (load-theme 'spacemacs-dark t))
@@ -68,6 +100,9 @@
   (require `spaceline-config)
   (spaceline-emacs-theme))
 
+;; Setup golang support
+(use-package go-mode)
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; General config
 (show-paren-mode 1)
