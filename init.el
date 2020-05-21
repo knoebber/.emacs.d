fef1b4903f8b40baacf35d16f2034213d7a24424
Nicolas Knoebber <knoebber@gmail.com>
2020-05-21 09:31:25 -0500

    Compile org-mode instead of using elpa

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
 
 ;;; Code:
 (require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (setq package-enable-at-startup nil)
@@ -215,6 +214,15 @@
   (interactive)
   (go-test (concat (go-current-function-name) "\b/" (go-sub-test-name) "\b/")))
 
+;; Compile org-mode:
+;; cd ~/packages
+;; git clone https://code.orgmode.org/bzg/org-mode.git
+;; cd org-mode
+;; git checkout maint (more stable than master)
+;; make autoloads
+(add-to-list 'load-path "~/packages/org-mode/lisp")
+(add-to-list 'load-path "~/packages/org-mode/contrib/lisp" t)
+
 (setq newsticker-url-list
       '(("EmacsWiki Recently Change" "http://www.emacswiki.org/cgi-bin/emacs?action=rss;showedit=1" nil nil nil)
         ("Devin Dooley" "https://devinadooley.com/index.xml" nil nil nil)
@@ -231,12 +239,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(line-number-mode nil)
  '(package-selected-packages
-   (quote
-    (htmlize php-mode dockerfile-mode company company-mode exec-path-from-shell spaceline spacemacs-theme use-package helm evil-visual-mark-mode))))
+   '(htmlize php-mode dockerfile-mode company company-mode exec-path-from-shell spaceline spacemacs-theme use-package helm evil-visual-mark-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
